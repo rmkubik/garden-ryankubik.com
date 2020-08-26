09e26384ab2110921efe14e62767a8e1a29806fa
Ryan Kubik <rkubik@twilio.com>
2020-08-26 13:20:01 -0700

    Link to feedback nights

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ReactDOMServer from "react-dom/server";
 
 import "./main.scss";
 import App from "./components/App";
+
+// https://reactjs.org/docs/react-dom-server.html
+// use the above react dom server to create a parcel plugin
+// read the pages directory
+// renderToString each page and create actual HTML files
+// then use hydrate instead of render below
+//
+// BIG Q - how do we render/hydrate individual pages for the router?
+// We could ditch the router and just create actual static pages?
 
 ReactDOM.render(<App />, document.getElementById("root"));
